70f426cbb3cea6cdc6ec3950e51e84383463ba4e
pramuditha <152759889+PramudithaB@users.noreply.github.com>
2026-02-08 08:49:49 +0530

    .

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -8,10 +8,7 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-Route::get('/', function () {
-    return file_get_contents(public_path('../dist/index.html'));
-});
+Route::get('/{any}', function () {
+    return file_get_contents(public_path('dist/index.html'));
+})->where('any', '.*');
 
-Route::get('/{path}', function () {
-    return file_get_contents(public_path('../dist/index.html'));
-})->where('path', '.*');
