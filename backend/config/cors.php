4801115c17ded7a3e41e3627c75de1984e222417
pramuditha <152759889+PramudithaB@users.noreply.github.com>
2026-02-11 08:26:57 +0530

    .

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,18 +1,12 @@
 <?php
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:3000',
         'https://admirus.com.lk',
         'https://www.admirus.com.lk',
     ],
