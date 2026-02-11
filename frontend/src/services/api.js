7ddf4a5d6141e0e67bd95a784ba5988fa8bdef30
pramuditha <152759889+PramudithaB@users.noreply.github.com>
2026-02-11 07:30:30 +0530

    .

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api', // Laravel API URL
+  baseURL: 'https://admirus.com.lk/api', // Laravel API URL
   withCredentials: true,
 });
 
